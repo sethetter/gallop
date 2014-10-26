173c8a4a2f59658311e2114e09be9cfce0ea9e24
Seth Etter <sethetter@gmail.com>
2014-10-25 22:19:29 -0500

    Hash password before validate

--- api/models/User.js
+++ api/models/User.js
@@ -5,6 +5,8 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var bcrypt = require('bcrypt');
+
 module.exports = {
 
   schema: true,
@@ -18,6 +20,22 @@ module.exports = {
       type: 'string',
       required: true
     }
+  },
+
+  beforeValidate: function(values, next) {
+
+    if (!values.password) return next();
+
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(values.password, salt, function(err, hash) {
+        if (err) return next(err);
+
+        values.password = hash;
+        next();
+      });
+    });
   }
 
 };
